60b52dbb3bf69da0b668cddb6f54227b082c4c1f
lliurex <juanma1980@gmail.com>
2023-02-06 09:16:32 +0100

    Delete tmpuser if present

--- src/lliuboot.sh
+++ src/lliuboot.sh
@@ -2,19 +2,17 @@
 
 fix_efi()
 {
-
-EFIDRIVE=''
-for i in $(lsblk | grep disk | cut -d " " -f1)
-do 
-	fdisk -l /dev/$i | grep EFI
-	if [ $? -eq 0 ]
-	then 
-		EFIDRIVE=/dev/$i
-		efibootmgr -c  -L "Windows Boot Manager" -l "\EFI\Boot\bootx64.efi" -d $EFIDRIVE
-		break
-	fi 
-done
-
+	EFIDRIVE=''
+	for i in $(lsblk | grep disk | cut -d " " -f1)
+	do 
+		fdisk -l /dev/$i | grep EFI
+		if [ $? -eq 0 ]
+		then 
+			EFIDRIVE=/dev/$i
+			efibootmgr -c  -L "Windows Boot Manager" -l "\EFI\Boot\bootx64.efi" -d $EFIDRIVE
+			break
+		fi 
+	done
 }
 
 set_bootnext()
@@ -23,6 +21,19 @@ set_bootnext()
 	[ ${#ID} -eq 4 ] && efibootmgr -n $ID
 }
 
+
+fix_users()
+{
+	USERF=$(</usr/share/lliuwin/.user)
+	if [ ! -z $USERF ]
+	then
+		userdel -r -f $USERF 2>/dev/null
+	fi
+}
+
+####
+
+fix_users
 efibootmgr | grep -i "windows"
 if [ $? -ne 0 ]
 then
